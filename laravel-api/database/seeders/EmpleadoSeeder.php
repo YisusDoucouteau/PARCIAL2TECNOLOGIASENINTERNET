f152e1871ae46d15b1d76ef253c4bf838fa0b5f9
YisusDoucouteau <92609918+YisusDoucouteau@users.noreply.github.com>
2025-10-27 21:04:00 -0400

    feat(api): crear migración tabla empleados

--- laravel-api/database/seeders/EmpleadoSeeder.php
+++ laravel-api/database/seeders/EmpleadoSeeder.php
@@ -9,11 +9,13 @@ class EmpleadoSeeder extends Seeder
 {
     public function run(): void
     {
+
         $data = [
             ['nombre'=>'juan','apellido'=>'dammer','correo'=>'agradecidocondios1@demo.com','salario'=>3500.00],
             ['nombre'=>'pedro','apellido'=>'berrios','correo'=>'agradecidocondios12demo.com','salario'=>4200.50],
             ['nombre'=>'francisco','apellido'=>'cuaquira','correo'=>'agradecidocondios41@demo.com','salario'=>3800.75],
         ];
+        
         foreach ($data as $e) { Empleado::create($e); }
     }
 }
