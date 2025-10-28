e31094e839888ff405043514d0afe8bc3cd2c9e9
YisusDoucouteau <92609918+YisusDoucouteau@users.noreply.github.com>
2025-10-27 21:06:45 -0400

    feat(api): crear modelo Empleado

--- laravel-api/app/Models/Empleado.php
+++ laravel-api/app/Models/Empleado.php
@@ -2,12 +2,14 @@
 
 namespace App\Models;
 
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 
 class Empleado extends Model
 {
+
     protected $table = 'empleados';
     protected $fillable = ['nombre','apellido','correo','salario'];
 }
