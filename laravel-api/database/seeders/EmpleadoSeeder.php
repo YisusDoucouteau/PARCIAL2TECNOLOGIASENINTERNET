5594c846953ac9b2583c8435c0badca5b61aadca
YisusDoucouteau <92609918+YisusDoucouteau@users.noreply.github.com>
2025-10-27 20:53:56 -0400

    chore: estructura inicial (Laravel + Angular creada)

---
+++ laravel-api/database/seeders/EmpleadoSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Empleado;
+use Illuminate\Database\Seeder;
+
+class EmpleadoSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $data = [
+            ['nombre'=>'juan','apellido'=>'dammer','correo'=>'agradecidocondios1@demo.com','salario'=>3500.00],
+            ['nombre'=>'pedro','apellido'=>'berrios','correo'=>'agradecidocondios12demo.com','salario'=>4200.50],
+            ['nombre'=>'francisco','apellido'=>'cuaquira','correo'=>'agradecidocondios41@demo.com','salario'=>3800.75],
+        ];
+        foreach ($data as $e) { Empleado::create($e); }
+    }
+}
